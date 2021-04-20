9d0fa15a1942da8c40c3e743218897d5b3a62a22
Nstellatos <nstellatos@yahoo.com>
2021-04-20 14:59:48 -0400

    created recipes table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_19_233500) do
+ActiveRecord::Schema.define(version: 2021_04_20_185557) do
+
+  create_table "recipes", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id", "created_at"], name: "index_recipes_on_user_id_and_created_at"
+    t.index ["user_id"], name: "index_recipes_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -24,4 +34,5 @@ ActiveRecord::Schema.define(version: 2021_04_19_233500) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "recipes", "users"
 end
