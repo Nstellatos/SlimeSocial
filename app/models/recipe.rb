48b5f1fbf425ef6404055061e8c9bcff21e3c139
Nstellatos <nstellatos@yahoo.com>
2021-04-20 15:13:15 -0400

    added default scope to recipes

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,6 @@
 class Recipe < ApplicationRecord
   belongs_to :user
+  default_scope -> { order(created_at: :desc) } #sets default order of recipes retrieved in db (newest to oldest)
 
   validates :user_id, presence: true
   validates :title, presence: true, length: {maximum: 120}
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
 
-  has_many :recipes 
+  has_many :recipes, dependent: :destroy #recipes are deleted when the user is destroyed
 end
