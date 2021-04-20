122e6e5f75d126e39d7c720091755e947837551f
Nstellatos <nstellatos@yahoo.com>
2021-04-20 15:04:55 -0400

    added validations to recipe

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,8 @@
 class Recipe < ApplicationRecord
   belongs_to :user
+
+  validates :user_id, presence: true
+  validates :title, presence: true, length: {maximum: 120}
+  validates :description, presence: true, length: {maximum: 260}
+
 end
