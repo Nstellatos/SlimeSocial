5d42277b99bb65faa0ffa1f978ff21b210678ac6
Nstellatos <nstellatos@yahoo.com>
2021-04-20 15:50:36 -0400

    added create action to recipes controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,31 @@
 class RecipesController < ApplicationController
+    before_action :authenticate_user!, only: [:create, :edit, :destroy]
+
+    def create 
+        @recipe = current_user.recipes.build(recipe_params)
+        if @recipe.save 
+            flash[:success] = "Recipe created"
+            redirect_to root_path
+        else
+            render 'new'
+        end
+    end
+
+
+    private 
+
+    def recipe_params 
+        params.require(:recipe).permit(:title, :description)
+    end 
+
+
+
+
+
+
+
+
+
+
+
 end
