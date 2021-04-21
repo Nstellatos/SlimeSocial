267ed35398e89a487739a7861a04e03ab7b03c2f
Nstellatos <nstellatos@yahoo.com>
2021-04-21 13:53:44 -0400

    added validations to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,20 @@
 class User < ApplicationRecord
 
+has_many :recipes 
+
+
+    validates :name, presence: true
+
+
+before_save { self.email = email.downcase }
+VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+validates :email, presence: true, length: { maximum: 200 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: true 
   
 
+has_secure_password
+    validates :password, presence: true, length: { minimum: 8 }, allow_nil: true
 
-    has_many :recipes 
-
-
-    has_secure_password
     
 end
