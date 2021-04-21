7bd00ada98e65b19547f1b4eb050d08142aef1e4
Nstellatos <nstellatos@yahoo.com>
2021-04-20 23:03:10 -0400

    added user_params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -31,7 +31,11 @@ class UsersController < ApplicationController
     def destroy
     end
 
+    private 
 
+    def user_params 
+        params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 
 
 
