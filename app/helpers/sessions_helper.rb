026dd4661212aa4270f25b5415152df9c23577ef
Nstellatos <nstellatos@yahoo.com>
2021-04-20 23:40:46 -0400

    implemented logic in sessions helper

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,26 @@
 module SessionsHelper
+
+    #logs in the given user
+    def log_in(user)
+        session[:user_id] = user.id
+    end
+
+
+    #returns the current logged in user
+    def current_user
+        if session[:user_id]
+            @current_user ||= User.find_by(id: session[:user_id])
+        end
+    end
+
+    #boolean to determin is a user is logged in
+    def user_logged_in?
+        !current_user.nil?
+    end
+
+    def log_out 
+        session.delete(:user_id)
+        @current_user = nil 
+    end
+
 end
