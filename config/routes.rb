0ac01fd5bfaa9942fb67458c6066c139623ede7a
Nstellatos <nstellatos@yahoo.com>
2021-04-20 20:35:33 -0400

    removed devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
-    sessions: 'users/sessions'
-  }
-  resources :users, :only => [:show]
+  
   resources :recipes
   root 'static_pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
