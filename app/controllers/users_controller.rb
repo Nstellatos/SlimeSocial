414dd8a43a39a6fb42a854bd34afae16a8563ee9
Nstellatos <nstellatos@yahoo.com>
2021-04-21 15:33:16 -0400

    working show page after login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+    before_action :logged_in_user, only: [ :edit, :update]
+    before_action :correct_user, only: [:edit, :update]
+
+
+
 
     def index 
         @users = User.all 
@@ -15,6 +20,7 @@ class UsersController < ApplicationController
     def create 
         @user = User.new(user_params)
         if @user.save 
+            log_in @user 
             flash[:success] = "Welcome to Slime Social!"
             redirect_to @user 
         else
@@ -22,10 +28,17 @@ class UsersController < ApplicationController
         end
     end
 
-    def edit 
+    def edit
+        @user = User.find(params[:id])
     end
 
     def update
+        if @user.update(user_params)
+            flash[:success] = "Profile updated"
+            redirect_to @user
+        else
+            render 'edit'
+        end
     end
 
     def destroy
@@ -36,6 +49,21 @@ class UsersController < ApplicationController
     def user_params 
         params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
+
+    #before filters
+
+    #confirms a logged in user
+    def logged_in_user
+        unless logged_in?
+            flash[:danger] = "please log in"
+            redirect_to login_url
+        end
+    end
+    #confirms the correct user
+    def correct_user 
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
+  end
 
 
 
