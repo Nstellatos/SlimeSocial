4352f2e1bff9671f41ef7d6127572a5a15f8262e
Nstellatos <nstellatos@yahoo.com>
2021-04-21 16:24:20 -0400

    destroy action and admin user filter

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-    before_action :logged_in_user, only: [ :edit, :update]
+    before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
     before_action :correct_user, only: [:edit, :update]
+    before_action :admin_user, only: [:destroy]
 
 
 
@@ -42,6 +43,9 @@ class UsersController < ApplicationController
     end
 
     def destroy
+        User.find(params[:id]).destroy
+        flash[:success] = "User deleted"
+        redirect_to users_url
     end
 
     private 
@@ -64,6 +68,11 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     redirect_to(root_url) unless current_user?(@user)
   end
+
+  #confirms an admin user
+    def admin_user
+    redirect_to(root_url) unless current_user.admin?
+    end
 
 
 
