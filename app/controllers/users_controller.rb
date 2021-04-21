61a0e5604f6bbfe42885417baefdb996e7751621
Nstellatos <nstellatos@yahoo.com>
2021-04-20 21:12:15 -0400

    added actions to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,31 @@
 class UsersController < ApplicationController
 
-def show 
-    @user = User.find(params[:id])
-end
+    def index 
+        @users = User.all 
+    end
+
+    def show 
+        @user = User.find(params[:id])
+    end
+
+    def new
+        @user = User.new
+    end
+
+    def create 
+    end
+
+    def edit 
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+    
+
+
+
 
 end
