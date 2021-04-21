64493bd84a5b6543a6acba9accb5b68b94940cce
Nstellatos <nstellatos@yahoo.com>
2021-04-20 20:48:20 -0400

    add index to users email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+
+  
+
+
+    has_many :recipes 
+
+    
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_21_002433) do
+ActiveRecord::Schema.define(version: 2021_04_21_004613) do
 
   create_table "recipes", force: :cascade do |t|
     t.string "title"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2021_04_21_002433) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "recipes", "users"
