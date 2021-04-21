1c9acd8171bf9ed8b0aef6b26aba1c53d3573634
Nstellatos <nstellatos@yahoo.com>
2021-04-21 17:50:53 -0400

    active storage install

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,6 @@
 class Recipe < ApplicationRecord
   belongs_to :user
+  has_many_attached :image
   default_scope -> { order(created_at: :desc) } #sets default order of recipes retrieved in db (newest to oldest)
 
   validates :user_id, presence: true
