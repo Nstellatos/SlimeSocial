2cbe9f6b13d78b7110766d2598827b018d78bbf9
Nstellatos <nstellatos@yahoo.com>
2021-04-21 18:32:16 -0400

    image validations

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -6,5 +6,13 @@ class Recipe < ApplicationRecord
   validates :user_id, presence: true
   validates :title, presence: true, length: {maximum: 120}
   validates :description, presence: true, length: {maximum: 260}
+  validates :image, content_type: { in: %w[image/jpeg image/gif image/png], 
+    message: "must be a valid image format"},
+    size: {less_than: 5.megabytes,
+    message: "should be less than 5MB" }
 
+    #returns a resized image
+    def display_image
+      image.variant(resize_to_limit: [500, 500])
+    end
 end
