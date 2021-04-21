828ae1b6236000ec98ada294829d14079689cb6e
Nstellatos <nstellatos@yahoo.com>
2021-04-20 23:20:50 -0400

    updated signup route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  
-  get 'sessions/new'
-  resources :recipes
+
   root 'static_pages#home'
+  get '/signup', to: 'users#new'
+  resources :recipes
+  
 
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
