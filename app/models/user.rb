7b43f0797c1eedbb9e015c031f59d8fff9f48bba
Nstellatos <nstellatos@yahoo.com>
2021-04-20 20:52:38 -0400

    added bcrypt/has_secure_password & password_digest

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,7 @@ class User < ApplicationRecord
 
     has_many :recipes 
 
+
+    has_secure_password
     
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_21_004613) do
+ActiveRecord::Schema.define(version: 2021_04_21_005036) do
 
   create_table "recipes", force: :cascade do |t|
     t.string "title"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2021_04_21_004613) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
