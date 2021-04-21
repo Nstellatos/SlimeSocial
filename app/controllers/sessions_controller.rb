ffe229f901567481552f1c0671c4a73833a76977
Nstellatos <nstellatos@yahoo.com>
2021-04-21 16:37:03 -0400

    added admin to users

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
   end
 
   def create 
-    user = User.find_by(email: params[:session][:email])
+    user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       #module helper to log a user in
       log_in user 
