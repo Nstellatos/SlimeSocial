ec5c854512e4af609ca3eacbe70318f879dd3a75
Nstellatos <nstellatos@yahoo.com>
2021-04-20 21:21:50 -0400

    created user views and routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   get 'sessions/new'
   resources :recipes
   root 'static_pages#home'
+
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
