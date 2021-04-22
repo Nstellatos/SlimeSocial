6c1f764df0eddc1840e805febbb4672db49175c7
Nstellatos <nstellatos@yahoo.com>
2021-04-22 17:46:47 -0400

    edit/update action working for recipes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,6 @@
 class RecipesController < ApplicationController
     before_action :logged_in_user, only: [:index, :show, :new, :create, :destroy]
-    before_action :correct_user, only: [:destroy]
+    before_action :correct_user, only: [:edit, :update, :destroy]
 
     def index 
         @recipes = Recipe.paginate(page: params[:page])
@@ -30,6 +30,18 @@ class RecipesController < ApplicationController
         end
     end
 
+    def edit 
+    end
+
+    def update
+        if @recipe.update(recipe_params)
+            redirect_to recipe_path(@recipe)
+        else
+            render 'edit'
+        end
+    end
+
+
     def destroy
         @recipe.destroy 
         flash[:success] = "slime deleted"
@@ -42,7 +54,7 @@ class RecipesController < ApplicationController
 
     def recipe_params 
         params.require(:recipe).permit(:title, :description, :image, 
-            recipe_ingredients_attributes: [:id, :ingredient_name, :_destroy])
+            recipe_ingredients_attributes: [:id, :name, :_destroy])
     end 
 
     def correct_user 
--- app/models/recipe_ingredient.rb
+++ app/models/recipe_ingredient.rb
@@ -5,23 +5,26 @@ class RecipeIngredient < ApplicationRecord
     accepts_nested_attributes_for :ingredient, :reject_if => :all_blank, :allow_destroy => true
 
 
+    def ingredient_name
+      self.ingredient.name if !self.id.nil?
+    end
 
-    def ingredient_attributes=(ingredient_attributes)
-      ingredient_attributes.values.each do |i|
-          if i != ""
-              ingredient = Ingredient.find_or_create_by(name: i.downcase)
-              self.ingredient = ingredient
-          end 
-      end 
-  end
+  #   def ingredient_attributes=(ingredient_attributes)
+  #     ingredient_attributes.values.each do |i|
+  #         if i != ""
+  #             ingredient = Ingredient.find_or_create_by(name: i.downcase)
+  #             self.ingredient = ingredient
+  #         end 
+  #     end 
+  # end
 end
 
     # def ingredient_name
     #     self.ingredient.name if !self.id.nil?
     #   end
     
-      # def ingredient_name=(name)
-      #   @ingredient = Ingredient.find_or_create_by(name: name)
-      #   self.ingredient = @ingredient
-      # end
+      def ingredient_name=(name)
+        @ingredient = Ingredient.find_or_create_by(name: name)
+        self.ingredient = @ingredient
+      end
 
