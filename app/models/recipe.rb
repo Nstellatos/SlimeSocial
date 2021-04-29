4c82ba8f0c94afff8d51ea7088340dcfe2ce5fdc
Nstellatos <nstellatos@yahoo.com>
2021-04-28 20:00:36 -0400

    recipe image updates

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,7 +2,7 @@ class Recipe < ApplicationRecord
   belongs_to :user
   has_many :recipe_ingredients, dependent: :destroy
   has_many :ingredients, through: :recipe_ingredients
-  has_one_attached :image
+  
   default_scope -> { order(created_at: :desc) } #sets default order of recipes retrieved in db (newest to oldest)
 
   accepts_nested_attributes_for :recipe_ingredients, :reject_if => :all_blank, :allow_destroy => true
@@ -11,13 +11,10 @@ class Recipe < ApplicationRecord
   validates :user_id, presence: true
   validates :title, presence: true, length: {maximum: 120}
   validates :description, presence: true, length: {maximum: 260}
-  validates :image, content_type: { in: %w[image/jpeg image/gif image/png], 
-    message: "must be a valid image format"},
-    size: {less_than: 5.megabytes,
-    message: "should be less than 5MB" }
+  
+  
+  has_attached_file :recipe_img, styles: { recipe_index: "250x350>", recipe_show: "325x475>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :recipe_img, content_type: /\Aimage\/.*\z/
 
-    #returns a resized image
-    def display_image
-      image.variant(resize_to_limit: [300, 300])
-    end
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_21_232207) do
+ActiveRecord::Schema.define(version: 2021_04_22_215209) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -52,6 +52,10 @@ ActiveRecord::Schema.define(version: 2021_04_21_232207) do
     t.integer "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "recipe_img_file_name"
+    t.string "recipe_img_content_type"
+    t.integer "recipe_img_file_size"
+    t.datetime "recipe_img_updated_at"
     t.index ["user_id", "created_at"], name: "index_recipes_on_user_id_and_created_at"
     t.index ["user_id"], name: "index_recipes_on_user_id"
   end
