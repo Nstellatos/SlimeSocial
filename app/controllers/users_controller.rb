4ef3e09a9b0064ef50a94f5ee8428b284210a61c
Nstellatos <nstellatos@yahoo.com>
2021-04-29 22:51:51 -0400

    ability to delete user account

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
     before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
-    before_action :correct_user, only: [:edit, :update]
-    before_action :admin_user, only: [:destroy]
+    before_action :correct_user, only: [:edit, :update, :destroy]
+    #before_action :admin_user, only: [:destroy]
 
 
 
@@ -66,10 +66,10 @@ class UsersController < ApplicationController
     redirect_to(root_url) unless current_user?(@user)
   end
 
-  #confirms an admin user
-    def admin_user
-    redirect_to(root_url) unless current_user.admin?
-    end
+#   #confirms an admin user
+#     def admin_user
+#     redirect_to(root_url) unless current_user.admin?
+#     end
 
 
 
